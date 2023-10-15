5c6768753ad6cb91bcde3218d77f5b2ff81e6f9a
olawunmi <olawunmiogunbiyi153@gmail.com>
2023-10-16 00:22:02 +0100

    simpleshell

---
+++ func.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - the function to count the string
+ * @s: the string to be countenate
+ *
+ * Return: Always void
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+
+
+	}
+	return (c);
+}
+
