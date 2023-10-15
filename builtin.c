46d4d6eb509edfa2b9239436193c3f940a25d2d6
Alfred Nti <87778148+Freddywhest@users.noreply.github.com>
2023-10-15 13:25:40 +0000

    Updated

--- builtin.c
+++ builtin.c
@@ -36,43 +36,38 @@ int _myexit(info_t *info)
  */
 int _mycd(info_t *info)
 {
-    char *dir, buffer[1024], *s = getcwd(buffer, 1024);
-    int chdir_ret;
-    if (!s)
-        _puts("TODO: >>getcwd failure emsg here<<\n");
-    if (!info->argv[1])
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
 	{
-        dir = _getenv(info, "HOME=");
-        chdir_ret = chdir((dir ? dir : _getenv(info, "PWD=")) ?: "/");
-    }
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: What should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-        if (!_getenv(info, "OLDPWD="))
+		if (!_getenv(info, "OLDPWD="))
 		{
-            _puts(s);
-            _putchar('\n');
-            return (1);
-        }
-        _puts(_getenv(info, "OLDPWD="));
-        _putchar('\n');
-        chdir_ret = chdir((_getenv(info, "OLDPWD=") ?: "/"));
-    }
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: What should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
 	else
 	{
-        chdir_ret = chdir(info->argv[1]);
-    }
-    if (chdir_ret == -1)
-	{
-        print_error(info, "can't cd to ");
-        _eputs(info->argv[1]);
-        _eputchar('\n');
-    }
-	else
-	{
-        _setenv(info, "OLDPWD", _getenv(info, "PWD="));
-        _setenv(info, "PWD", getcwd(buffer, 1024));
-    }
-    return (0);
+		chdir_ret = chdir(info->argv[1]);
+	}
+	return (0);
 }
 
 /**
