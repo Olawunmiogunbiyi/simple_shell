bc36c11efe81df7f8bdf70fd47dc2fc227cf1042
Alfred Nti <87778148+Freddywhest@users.noreply.github.com>
2023-10-14 17:15:10 -0700

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,62 +1,74 @@
 #include "shell.h"
-/**
- * exitshell - A function to exit the shell program
- * @tokk : string received from getline
- * @line: program line pointer
- *
- * return: void
- */
-void exitshell(char **tokk, char *line)
-{
-	int status = EXIT_SUCCESS;
 
-	if (tokk[1])
-	{
-		status = _atoi(tokk[1]);
-		if (status > 0)
-		{
-			free_vector(tokk);
-			free(line);
-			exit(status);
-		}
-		else
-		{
-			status = 2;
-			perror("2: Illegal number");
-			free(line);
-			free_vector(tokk);
-			exit(status);
-		}
-	}
-	free_vector(tokk);
-	free(line);
-	exit(status);
-}
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-char *_strcat(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i, j;
+	char *s = dest;
 
 	i = 0;
-
-	while (dest[i] != '\0')
+	while (src[i] != '\0' && i < n - 1)
 	{
+		dest[i] = src[i];
 		i++;
-		j = 0;
 	}
-	while (src[j] != '\0')
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
